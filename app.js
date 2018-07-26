f68fe0921c3b700e8a8299dbe5e1b2da824f2472
Treasure <treasure.rose@gmail.com>
2018-07-26 16:44:46 -0700

    clean up

--- app.js
+++ app.js
@@ -11,15 +11,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-  res.render('about', { title: 'About'});
-});
-
-app.get('/contact', function (req, res) {
-  res.render('contact', { title: 'Contact'});
+  res.render('about');
 });
 
 app.get('/projects', function (req, res) {
-  res.render('project', { title: 'Project'});
+  res.render('project');
 });
 
 app.get('/projects/:projectID', function (req, res) {
